fdc6d46d1060c09d3f545ccc8f3ec17163fb8a99
KaanArslan19 <kaanarslan35@gmail.com>
2023-11-06 17:50:35 +0300

    404 page added

---
+++ components/constants/Error.jsx
@@ -0,0 +1,18 @@
+import { Container } from "@chakra-ui/react";
+import Image from "next/image";
+import classes from "./Error.module.scss";
+const Error = () => {
+  return (
+    <Container centerContent mt={"2rem"} maxW={{ base: "768px", xl: "992px" }}>
+      <Image
+        src="/images/404error.jpg"
+        alt="404-error"
+        fill={true}
+        style={{ objectFit: "contain" }}
+        className={classes.img}
+      />
+    </Container>
+  );
+};
+
+export default Error;
--- pages/404.js
+++ pages/404.js
@@ -1,38 +1,38 @@
-import Contact from "@/components/constants/Contact";
 import Head from "next/head";
-import { serverSideTranslations } from "next-i18next/serverSideTranslations";
+import { Fragment } from "react";
 import { useTranslation } from "next-i18next";
-
-export default function AboutPage() {
+import { serverSideTranslations } from "next-i18next/serverSideTranslations";
+import Error from "@/components/constants/Error";
+const Page404 = () => {
   const { t: translate } = useTranslation("meta");
 
   return (
-    <>
+    <Fragment>
       <Head>
         <meta property="og:image" content="/images/ars_concept_logo.png" />
         <meta property="og:image:width" content="200px" />
         <meta property="og:image:height" content="200px" />
         <meta property="og:image:alt" content={translate("meta:home.title")} />
         <link rel="canonical" href="https://www.arsconcepthouses.com/contact" />
-        <title>{translate("meta:contact.title")}</title>
-        <meta name="description" content={translate("meta:contact.content")} />
+        <title>{translate("meta:error.title")}</title>
+        <meta name="description" content={translate("meta:error.content")} />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon_ars.ico" />
       </Head>
-      <Contact />
-    </>
+      <Error />
+    </Fragment>
   );
-}
+};
+
+export default Page404;
 
 export async function getStaticProps({ locale }) {
   return {
     props: {
-      ...(await serverSideTranslations(
-        locale,
-        ["home", "contact", "meta"],
-        null,
-        ["en", "tr"]
-      )),
+      ...(await serverSideTranslations(locale, ["home", "meta"], null, [
+        "en",
+        "tr",
+      ])),
     },
   };
 }
